c26ee72f74f8086f7f137e290f8002325e97b137
Raul Langle <ra04navi@outlook.com>
2026-04-08 13:26:29 -0600

    feat: new navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header class="navbar">
+        <div class="nav-content">
+            <h2 class="logo">One Piece <span>Wiki</span></h2>
+            <nav>
+                <router-link to="/">Inicio</router-link>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+.navbar {
+    background-color: #1a1a1a;
+    color: white;
+    padding: 1rem 2rem;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    position: sticky;
+    top: 0;
+    z-index: 100;
+}
+
+.nav-content {
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.logo {
+    margin: 0;
+    font-size: 1.5rem;
+}
+
+.logo span {
+    color: #fbbf24;
+    /* Un amarillo tipo oro pirata */
+}
+
+nav a {
+    color: white;
+    text-decoration: none;
+    font-weight: bold;
+    padding: 0.5rem 1rem;
+    border-radius: 4px;
+    transition: background-color 0.2s;
+}
+
+nav a:hover,
+nav a.router-link-active {
+    /* Vue Router añade automáticamente la clase .router-link-active al enlace actual */
+    background-color: #333;
+    color: #fbbf24;
+}
+</style>
